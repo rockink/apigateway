941743ebb63ea6e8f063ed7ef45ad9ad8f5c59b4
Nirmal Nepal <rockink_nirmal@live.com>
2019-03-25 17:36:53 -0400

    Pre Filter that logs the URL and the parameters

--- src/main/kotlin/com/test/apigteway/ApigtewayApplication.kt
+++ src/main/kotlin/com/test/apigteway/ApigtewayApplication.kt
@@ -1,9 +1,13 @@
 package com.test.apigteway
 
+import com.test.apigteway.preFilters.LogFilter
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
+@EnableZuulProxy
 class ApigtewayApplication
 
 fun main(args: Array<String>) {
