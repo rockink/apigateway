63cade206c84e94cd3b896cc64b20305e139eb51
Nirmal Nepal <rockink_nirmal@live.com>
2019-03-25 16:40:03 -0400

    init project

---
+++ README.md
@@ -0,0 +1,10 @@
+## ApiGateway
+This project is supposed to be an API-Gateway for the shopping app. 
+Behind this api-gateway, there are microservices this api-gateway 
+is supposed to call. 
+
+## Developing it 
+This is done via `mvn spring-boot:run` command, or running through IDE. 
+
+
+
---
+++ src/main/kotlin/com/test/apigteway/ApigtewayApplication.kt
@@ -0,0 +1,11 @@
+package com.test.apigteway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApigtewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApigtewayApplication>(*args)
+}
